f4fe48196711475ef24fed78bec3976e4529a365
Victor-Barbosa <victorbarbosadev@gmail.com>
2021-07-06 15:23:32 -0300

    setup do projeto

---
+++ src/main/java/br/com/zupacademy/victor/casadocodigo/OrangeTalents06TemplateCasaDoCodigoApplication.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.victor.casadocodigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrangeTalents06TemplateCasaDoCodigoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrangeTalents06TemplateCasaDoCodigoApplication.class, args);
+    }
+
+}
