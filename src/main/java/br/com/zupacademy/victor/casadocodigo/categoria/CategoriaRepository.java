1378a3530d19cff5095b43fed573acbca4aa91f9
Victor Hugo Barbosa Silva <78871930+Victor-Barbosa@users.noreply.github.com>
2021-07-07 12:19:26 -0300

    Delete src/main/java/br/com/zupacademy/victor/casadocodigo/categoria directory

--- src/main/java/br/com/zupacademy/victor/casadocodigo/categoria/CategoriaRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.zupacademy.victor.casadocodigo.categoria;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
-    boolean existsByNome(String nome);
-}
