f4e7267ba6e609775193f6204761f2b9f174c40e
Victor-Barbosa <victorbarbosadev@gmail.com>
2021-07-07 12:17:40 -0300

    Revert "Cadastro Categoria"
    This reverts commit 5118ce06

--- src/main/java/br/com/zupacademy/victor/casadocodigo/Autor/AutorController.java
+++ src/main/java/br/com/zupacademy/victor/casadocodigo/Autor/AutorController.java
@@ -1,5 +1,6 @@
 package br.com.zupacademy.victor.casadocodigo.Autor;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
